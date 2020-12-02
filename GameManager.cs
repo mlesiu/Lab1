a5089170b5aa0ae013ee38294507a28b80e58368
mlesiu <75387549+mlesiu@users.noreply.github.com>
2020-12-02 19:28:49 +0100

    Skrypty
    Skrypty

---
+++ GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public int coins = 0;
+    public int lifes = 3;
+
+    public void AddCoins()
+    {
+        coins += 1;
+     
+    }
+
+    public void Dead()
+    {
+        lifes -= 1;
+        if (lifes <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
---
+++ Health.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Text health;
+    GameManager gameManager;
+
+    
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+   
+    void Update()
+    {
+        health.text = "Lifes: " + gameManager.lifes;
+    }
+}
